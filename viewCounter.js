ccd13914ab51fc39e79e56b781868ef2c38676d4
RubenParnell2 <145464795+RubenParnell2@users.noreply.github.com>
2024-03-15 12:58:41 +0000

    Update viewCounter.js

--- viewCounter.js
+++ viewCounter.js
@@ -1,4 +1,5 @@
 function liveViews(response) {
     document.getElementById('visits').innerText = response.value;
+    console.log(document.getElementById('visits').innerText = response.value);
 }
 // The function is called in the CDN on HTML.
