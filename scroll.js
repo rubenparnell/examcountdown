fc514f211e01abdc4461c5d0b92d938e7e285fcc
rubenparnell <66206505+rubenparnell@users.noreply.github.com>
2024-04-16 18:44:34 +0100

    Update distances

--- scroll.js
+++ scroll.js
@@ -4,8 +4,8 @@ const sideMenu = document.getElementById('side-menu');
 const spacing = document.getElementById('spacing');
 
 function handleScroll() {
-    const isLargeScreen = window.matchMedia("(min-width: 1291px)").matches;
-    const isMediumScreen = window.matchMedia("(max-width: 1290px)").matches;
+    const isLargeScreen = window.matchMedia("(min-width: 1491px)").matches;
+    const isMediumScreen = window.matchMedia("(max-width: 1491px)").matches;
     const isSmallScreen = window.matchMedia("(max-width: 536px)").matches;
 
     let spacingPadding = isSmallScreen ? "30px" : isMediumScreen ? "10px" : "0px";
